836f0e529f4d0393f56d0311fd4a0b79a746cbe4
Benjamin Lutz <benjamin.lutz@gmail.com>
2015-05-01 08:57:19 +0200

    generated project with yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name timaxjsClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the timaxjsClientApp
+ */
+angular.module('timaxjsClientApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
