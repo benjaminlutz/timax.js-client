557141725ac17a5f1aa6819d5fa6e4882b2c187c
Benjamin Lutz <benjamin.lutz@gmail.com>
2015-05-14 13:16:06 +0200

    added type ahead to manage user dialog

--- app/scripts/controllers/modals/manageUser.controller.js
+++ app/scripts/controllers/modals/manageUser.controller.js
@@ -4,10 +4,20 @@
     /**
      * Manage user controller.
      */
-    angular.module('timax.controllers.modals.manageUser', [])
+    angular.module('timax.controllers.modals.manageUser', ['timax.services.user'])
 
-        .controller('ManageUserController', function ($scope, $element, project, close) {
+        .controller('ManageUserController', function ($scope, $element, userService, project, close) {
             $scope.project = project;
+
+            $scope.selected = undefined;
+
+            $scope.addButtonDisabled = function () {
+                return angular.isObject($scope.selected) === false;
+            };
+
+            $scope.getUser = function (searchString) {
+                return userService.searchUser(searchString);
+            };
 
             $scope.close = function (result) {
                 close(result, 500);
---
+++ app/scripts/services/user.service.js
@@ -0,0 +1,33 @@
+(function () {
+    'use strict';
+
+    /**
+     * User service.
+     */
+    angular.module('timax.services.user', ['timax.config'])
+
+        .constant('USER_RESOURCE_URL', 'user')
+
+        .factory('userService', function ($q, $http, timaxConfig, USER_RESOURCE_URL) {
+            var factoryObject = {};
+
+            factoryObject.searchUser = function (searchString) {
+                var deferred = $q.defer(),
+                    searchUserUrl = '/search?q=' + searchString;
+
+                $http.get(timaxConfig.BACKEND + USER_RESOURCE_URL + searchUserUrl).then(function (response) {
+                    for (var i = 0; i < response.data.length; i++) {
+                        var user = response.data[i];
+                        user.formattedName = user.firstName + ' ' + user.lastName + ' (' + user.email + ')';
+                    }
+                    deferred.resolve(response.data);
+                }, function (err) {
+                    deferred.reject(err);
+                });
+
+                return deferred.promise;
+            };
+
+            return factoryObject;
+        });
+})();
