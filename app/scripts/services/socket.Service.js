f5b41ecd9e4022eaef1f2bd2fd8250e8d6a1a287
Benjamin Lutz <benjamin.lutz@gmail.com>
2015-06-02 08:45:02 +0200

    socket service only connects with role admin or manager

--- app/scripts/services/socket.Service.js
+++ app/scripts/services/socket.Service.js
@@ -4,19 +4,22 @@
     /**
      * Socket service.
      */
-    angular.module('timax.services.socket', ['btford.socket-io', 'timax.config'])
+    angular.module('timax.services.socket', ['btford.socket-io', 'LocalStorageModule', 'timax.services.authorisation', 'timax.config'])
 
-        .factory('socketService', function ($q, $http, socketFactory, localStorageService, timaxConfig) {
-            var token = localStorageService.get('token');
+        .factory('socketService', function ($q, $http, socketFactory, localStorageService, authorisationService, timaxConfig) {
+            var token = localStorageService.get('token'),
+                principal = authorisationService.getPrincipal();
 
-            // TODO check authentication, role, etc...
-
-            var timaxSocket = socketFactory({
-                ioSocket: io.connect(timaxConfig.SOCKET, {
-                    query: 'token=' + token
-                })
-            });
-            timaxSocket.forward('booking');
-            return timaxSocket;
+            if (angular.isObject(principal) && (principal.role === 'admin' || principal.role === 'manager')) {
+                var timaxSocket = socketFactory({
+                    ioSocket: io.connect(timaxConfig.SOCKET, {
+                        query: 'token=' + token
+                    })
+                });
+                timaxSocket.forward('booking');
+                return timaxSocket;
+            } else {
+                return false;
+            }
         });
 })();
